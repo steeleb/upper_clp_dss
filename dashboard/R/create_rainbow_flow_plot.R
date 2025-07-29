c950c7501a8bcfa4a1e171fdc6d9e6706f6907e0
SamStruthers <sam.struthers2000@gmail.com>
2025-07-29 09:26:01 -0600

    fixing indentations, adding package requires, removing dplyr and cdssr:: and group by issues

--- dashboard/R/create_rainbow_flow_plot.R
+++ dashboard/R/create_rainbow_flow_plot.R
@@ -63,227 +63,222 @@
 
 create_rainbow_flow_plot <- function(station_abbrev = NULL, station_plot_name = NULL, years = 30, incl_winter = T, min_val = 0.1){
 
-  library(cdssr)
-  library(tidyverse)
+  #loading packages
+  if (!require(cdssr, quietly = TRUE)) {
+    library(cdssr)
+  }
+  if (!require(tidyverse, quietly = TRUE)) {
+    library(tidyverse)
+  }
+  if (!exists("%nin%")) {
+    `%nin%` = Negate(`%in%`)
+  }
 
-  `%nin%` = Negate(`%in%`)
 
-    #find the min date of the X years ago
-    start_date = paste0((year(Sys.Date()) - as.numeric(years)), "-01-01")
-    #find the max date of this year
-    end_date = paste0((year(Sys.Date())), "-12-31")
+  #find the min date of the X years ago
+  start_date = paste0((year(Sys.Date()) - as.numeric(years)), "-01-01")
+  #find the max date of this year
+  end_date = paste0((year(Sys.Date())), "-12-31")
 
-
-    flow_data <- cdssr::get_sw_ts(
-      abbrev = station_abbrev,
-      start_date = start_date,
-      end_date = end_date,
-      timescale = "day")%>%
-      dplyr::select(datetime, value, flag_a, flag_b, flag_c, flag_d)%>%
-      mutate(year = year(datetime),
-             date = format(datetime, "%m-%d"),
-             value = ifelse(value <= min_val, min_val, value))%>%
-      #remove any flags which include the tag ice
-      filter(!grepl("ice", flag_a, ignore.case = TRUE) &
+  #Get flow data for site over the last X years
+  flow_data <- get_sw_ts(
+    abbrev = station_abbrev,
+    start_date = start_date,
+    end_date = end_date,
+    timescale = "day")%>%
+    select(datetime, value, flag_a, flag_b, flag_c, flag_d)%>%
+    #clean up dates and filter values to be greater than min val
+    mutate(year = year(datetime),
+           date = format(datetime, "%m-%d"),
+           value = ifelse(value <= min_val, min_val, value))%>%
+    #remove any flags which include the tag ice
+    filter(!grepl("ice", flag_a, ignore.case = TRUE) &
              !grepl("ice", flag_b, ignore.case = TRUE) &
              !grepl("ice", flag_c, ignore.case = TRUE) &
              !grepl("ice", flag_d, ignore.case = TRUE))
 
-    if(incl_winter == F){
-      # Filter out winter months (December, January, February)
-      flow_data <- flow_data %>%
-        filter(!month(datetime) %in% c(12, 1, 2))
-    }
+  if(incl_winter == F){
+    # Filter out winter months (December, January, February)
+    flow_data <- flow_data %>%
+      filter(!month(datetime) %in% c(12, 1, 2))
+  }
 
-    #browser()
-    #calculate min, median, max, Q10, Q30, Q70 and Q90 for each day of year
+  #calculate min, median, max, Q10, Q30, Q70 and Q90 for each day of year
 
-    wide_flow_stats <- flow_data %>%
-      group_by(date) %>%
-      summarise(
-        min = min(value, na.rm = TRUE),
-        median = median(value, na.rm = TRUE),
-        max = max(value, na.rm = TRUE),
-        Q10 = as.numeric(quantile(value, probs = 0.1, na.rm = TRUE)),
-        Q30 = as.numeric(quantile(value, probs = 0.3, na.rm = TRUE)),
-        Q70 = as.numeric(quantile(value, probs = 0.7, na.rm = TRUE)),
-        Q90 = as.numeric(quantile(value, probs = 0.9, na.rm = TRUE))
-      ) %>%
-      ungroup()
+  wide_flow_stats <- flow_data %>%
+    summarise(
+      Min = min(value, na.rm = TRUE),
+      Median = median(value, na.rm = TRUE),
+      Max = max(value, na.rm = TRUE),
+      `10th Percentile` = as.numeric(quantile(value, probs = 0.1, na.rm = TRUE)),
+      `30th Percentile` = as.numeric(quantile(value, probs = 0.3, na.rm = TRUE)),
+      `70th Percentile` = as.numeric(quantile(value, probs = 0.7, na.rm = TRUE)),
+      `90th Percentile` = as.numeric(quantile(value, probs = 0.9, na.rm = TRUE))
+      , .by = date)
 
-#grab current year
-cur_year = year(Sys.Date())
-recent_date = Sys.Date() - days(1)
+  #grab current year
+  cur_year = year(Sys.Date())
+  recent_date = Sys.Date() - days(1)
 
-# Reshape the data from wide to long format
-non_stats <- flow_data%>%
-  dplyr::select(date, flow = value, datetime)%>%
-  mutate(month_day = date,
-         year = year(datetime),
-         date = ymd(paste0("2000-", month_day)),
-         full_date = as.Date(paste0(as.numeric(cur_year), "-", month_day), format = "%Y-%m-%d"))  # Ensure low/negative flows are set to 0.1 for plotting
-#set aside current data
-curr_year <- non_stats%>%
-  filter(year == as.character(cur_year))%>%
-  dplyr::select(date, flow, full_date)
+  # grab all data and reshape date for same axes plotting
+  non_stats <- flow_data%>%
+    select(date, flow = value, datetime)%>%
+    mutate(month_day = date,
+           year = year(datetime),
+           #setting date to 2000-month-day formatting for plotting on same axes
+           date = ymd(paste0("2000-", month_day)),
+           full_date = as.Date(paste0(as.numeric(cur_year), "-", month_day), format = "%Y-%m-%d"))
 
-#grab yesterdays flow
-cur_flow <- curr_year %>%
-  filter(full_date == recent_date) %>%
-  pull(flow)
+  #set aside current year's data
+  curr_year <- non_stats%>%
+    filter(year == as.character(cur_year))%>%
+    select(date, flow, full_date)
 
+  #grab yesterdays flow
+  cur_flow <- curr_year %>%
+    filter(full_date == recent_date) %>%
+    pull(flow)
 
+  # If yesterday is not available, try to find the most recent flow data (within a week)
+  if(is_empty(cur_flow)){
+    # Try up to 7 days back to find data
+    for(i in 2:8) {
+      cur_flow <- curr_year %>%
+        filter(full_date == Sys.Date() - days(i)) %>%
+        pull(flow)
 
-if(is_empty(cur_flow)){
-  # Try up to 7 days back to find data
-  for(i in 2:8) {
-    cur_flow <- curr_year %>%
-      filter(full_date == Sys.Date() - days(i)) %>%
-      pull(flow)
-
-    if(!is_empty(cur_flow)) {
-      break  # Exit loop when we find data
-      recent_date = Sys.Date() - days(i)
+      if(!is_empty(cur_flow)) {
+        break  # Exit loop when we find data
+        recent_date = Sys.Date() - days(i)
+      }
     }
   }
-}
-# grab the 6 most recent years
-recent_years <- non_stats%>%
-  filter(year <= as.character(cur_year-1) & year >= as.character(cur_year-6))%>%
-  dplyr::select(date, flow, full_date, year)
 
-# Extract statistical columns
-stats_data <- wide_flow_stats %>%
-  pivot_longer(
-    cols = -date,
-    names_to = "stat",
-    values_to = "flow"
-  ) %>%
-  mutate(stat = case_when(
-    stat == "min" ~ "Min",
-    stat == "median" ~ "Median",
-    stat == "max" ~ "Max",
-    stat == "Q10" ~ "10th Percentile",
-    stat == "Q30" ~ "30th Percentile",
-    stat == "Q70" ~ "70th Percentile",
-    stat == "Q90" ~ "90th Percentile"))%>%
-  filter(!is.na(stat)&!is.na(flow))%>%
-  mutate(month_day = date,
-         date = ymd(paste0("2000-", date)),
-         full_date = as.Date(paste0(as.numeric(cur_year), "-", month_day), format = "%Y-%m-%d"),
-         flow = ifelse(flow <= 0.1, 0.1, flow)) # Ensure low/negative flows are set to 0.1 for plotting
+  # grab the 5 most recent years
+  recent_years <- non_stats%>%
+    filter(year <= as.character(cur_year-1) & year >= as.character(cur_year-5))%>%
+    dplyr::select(date, flow, full_date, year)
 
-percentile_ribbons <- stats_data %>%
-  pivot_wider(names_from = stat, values_from = flow)
+  # Extract statistical columns
+  stats_data <- wide_flow_stats %>%
+    pivot_longer(
+      cols = -date,
+      names_to = "stat",
+      values_to = "flow"
+    ) %>%
+    filter(!is.na(stat)&!is.na(flow))%>%
+    mutate(month_day = date,
+           date = ymd(paste0("2000-", date)),
+           full_date = as.Date(paste0(as.numeric(cur_year), "-", month_day), format = "%Y-%m-%d"),
+           flow = ifelse(flow <= min_val, min_Val, flow)) %>% # Ensure low/negative flows are set to 0.1 for plotting
+    arrange(full_date, stat)
 
-#grab the median percent
+  percentile_ribbons <- stats_data %>%
+    pivot_wider(names_from = stat, values_from = flow)
 
-perc_med = stats_data%>%
-  filter(stat == "Median" & full_date == recent_date)%>%
-  mutate(perc_med = round((cur_flow/flow)*100, 0))%>%
-  pull(perc_med)
+  #grab the median percent
 
+  perc_med <- stats_data%>%
+    filter(stat == "Median" & full_date == recent_date)%>%
+    mutate(perc_med = round((cur_flow/flow)*100, 0))%>%
+    pull(perc_med)
 
-#create a plotly line plot of each stat over a year
-stats_plot <- plot_ly()%>%
-  add_ribbons(
-    data = percentile_ribbons,
-    x = ~full_date,
-    ymin = ~`Min`,
-    ymax = ~`10th Percentile`,
-    fillcolor = 'rgba(255,0, 0, 0.2)',
-    line = list(color = 'transparent'),
-    showlegend = FALSE
-  )%>%
-  add_ribbons(
-    data = percentile_ribbons,
-    x = ~full_date,
-    ymin = ~`10th Percentile`,
-    ymax = ~`30th Percentile`,
-    fillcolor = 'rgba(255,162, 0, 0.2)',
-    line = list(color = 'transparent'),
-    showlegend = FALSE
-  )%>%
-  add_ribbons(
-    data = percentile_ribbons,
-    x = ~full_date,
-    ymin = ~`30th Percentile`,
-    ymax = ~`70th Percentile`,
-    fillcolor = 'rgba(0,227, 116, 0.2)',
-    line = list(color = 'transparent'),
-    showlegend = FALSE
-  )%>%
-  add_ribbons(
-    data = percentile_ribbons,
-    x = ~full_date,
-    ymin = ~`70th Percentile`,
-    ymax = ~`90th Percentile`,
-    fillcolor = 'rgba(0,255, 252, 0.2)',
-    line = list(color = 'transparent'),
-    showlegend = FALSE
-  )%>%
-  add_ribbons(
-    data = percentile_ribbons,
-    x = ~full_date,
-    ymin = ~`90th Percentile`,
-    ymax = ~`Max`,
-    fillcolor = 'rgba(0,109,255, 0.2)',
-    line = list(color = 'transparent'),
-    showlegend = FALSE
-  )%>%
-  add_trace(
-    data = stats_data%>%filter(stat %nin% c("10th Percentile", "30th Percentile", "70th Percentile", "90th Percentile")),
-    x = ~full_date,
-    y = ~flow,
-    color =  ~stat,
-    type = "scatter",
-    mode = "lines",
-    line = list(width = 2),
-    colors = c("Min" = "rgba(255,0, 0, 0.8)", "Median" = "rgba(0,227, 116, 0.8)", "Max" = "rgba(0,109,255, 0.8)")) %>%
-  add_trace(
-    data = curr_year,
-    x = ~full_date, y = ~flow, type = "scatter", mode = "lines",
-    line = list(color = "black", width = 2),
-    name = paste0("Current Year (", cur_year, ")"),
-    showlegend = TRUE
-  ) %>%
-  add_trace(
-    data = recent_years,
-    x = ~full_date, y = ~flow, type = "scatter", mode = "lines",
-    split = ~factor(year, levels = rev(unique(recent_years$year))),
-    line = list(width = 0.5),
-    showlegend = TRUE,
-    name = ~factor(year, levels = rev(unique(recent_years$year))), visible="legendonly"
-  ) %>%
-  layout(
-    title = paste0(station_plot_name, " Flow Statistics"),
-    xaxis = list(title = "Date"),
-    yaxis = list(title = "Flow (cfs)"),
-    showlegend = TRUE) %>%
-  layout(
-    title = "",
-    xaxis = list(title = "",tickformat = "%b %d",range = c(min(stats_data$full_date, na.rm = T),max(stats_data$full_date, na.rm = T))
-    ),
-    yaxis = list(title = "Flow (cfs)",type = "log"),
-    legend = list(x = 1, y = 0.95,title = list(text = "")),
-    annotations = list(
-      list(
-        x = .70, y = .95,
-        xref = "paper", yref = "paper",
-        #x = min(stats_data$full_date, na.rm = T) + 15, y = log(max(stats_data$flow, na.rm = T)*.005),
-        text = paste0(
-          "Flow data for ",station_plot_name, "<br>",
-          "Years of data: ", years, "<br>",
-          "Current as of ", recent_date, ":<br>",
-          "Current Basin Flow: ", cur_flow, "(cfs)", "<br>"
-        ),
-        showarrow = FALSE,bordercolor = "black",borderwidth = 1,bgcolor = "white",xanchor = "left"
-      )
-    ),
-    margin = list(t = 40, r = 100, b = 40, l = 60)
-  )
+  #create a plotly line plot of each stat over a year
+  stats_plot <- plot_ly()%>%
+    add_ribbons(
+      data = percentile_ribbons,
+      x = ~full_date,
+      ymin = ~`Min`,
+      ymax = ~`10th Percentile`,
+      fillcolor = 'rgba(255,0, 0, 0.2)',
+      line = list(color = 'transparent'),
+      showlegend = FALSE
+    )%>%
+    add_ribbons(
+      data = percentile_ribbons,
+      x = ~full_date,
+      ymin = ~`10th Percentile`,
+      ymax = ~`30th Percentile`,
+      fillcolor = 'rgba(255,162, 0, 0.2)',
+      line = list(color = 'transparent'),
+      showlegend = FALSE
+    )%>%
+    add_ribbons(
+      data = percentile_ribbons,
+      x = ~full_date,
+      ymin = ~`30th Percentile`,
+      ymax = ~`70th Percentile`,
+      fillcolor = 'rgba(0,227, 116, 0.2)',
+      line = list(color = 'transparent'),
+      showlegend = FALSE
+    )%>%
+    add_ribbons(
+      data = percentile_ribbons,
+      x = ~full_date,
+      ymin = ~`70th Percentile`,
+      ymax = ~`90th Percentile`,
+      fillcolor = 'rgba(0,255, 252, 0.2)',
+      line = list(color = 'transparent'),
+      showlegend = FALSE
+    )%>%
+    add_ribbons(
+      data = percentile_ribbons,
+      x = ~full_date,
+      ymin = ~`90th Percentile`,
+      ymax = ~`Max`,
+      fillcolor = 'rgba(0,109,255, 0.2)',
+      line = list(color = 'transparent'),
+      showlegend = FALSE
+    )%>%
+    add_trace(
+      data = stats_data%>%filter(stat %nin% c("10th Percentile", "30th Percentile", "70th Percentile", "90th Percentile")),
+      x = ~full_date,
+      y = ~flow,
+      color =  ~stat,
+      split = ~stat,
+      type = "scatter",
+      mode = "lines",
+      line = list(width = 2),
+      colors = c("Min" = "rgba(255,0, 0, 0.8)", "Median" = "rgba(0,227, 116, 0.8)", "Max" = "rgba(0,109,255, 0.8)")) %>%
+    add_trace(
+      data = curr_year,
+      x = ~full_date, y = ~flow, type = "scatter", mode = "lines",
+      line = list(color = "black", width = 2),
+      name = paste0("Current Year (", cur_year, ")"),
+      showlegend = TRUE
+    ) %>%
+    add_trace(
+      data = recent_years,
+      x = ~full_date, y = ~flow, type = "scatter", mode = "lines",
+      split = ~factor(year, levels = rev(unique(recent_years$year))),
+      line = list(width = 0.5),
+      showlegend = TRUE,
+      name = ~factor(year, levels = rev(unique(recent_years$year))), visible="legendonly"
+    ) %>%
+    layout(
+      title = "",
+      xaxis = list(title = "",tickformat = "%b %d",range = c(min(stats_data$full_date, na.rm = T),max(stats_data$full_date, na.rm = T))
+      ),
+      yaxis = list(title = "Flow (cfs)",type = "log"),
+      legend = list(x = 1, y = 0.95,title = list(text = "")),
+      annotations = list(
+        list(
+          x = .70, y = .95,
+          xref = "paper", yref = "paper",
+          #x = min(stats_data$full_date, na.rm = T) + 15, y = log(max(stats_data$flow, na.rm = T)*.005),
+          text = paste0(
+            "Flow data for ",station_plot_name, "<br>",
+            "Years of data: ", years, "<br>",
+            "Current as of ", recent_date, ":<br>",
+            "Current Basin Flow: ", cur_flow, "(cfs)", "<br>"
+          ),
+          showarrow = FALSE,bordercolor = "black",borderwidth = 1,bgcolor = "white",xanchor = "left"
+        )
+      ),
+      margin = list(t = 40, r = 100, b = 40, l = 60)
+    )
 
-stats_plot
+  stats_plot
 }
 
 
