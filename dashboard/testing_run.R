254ac1659fa504e35873a9abc1a6f24a332233f2
SamStruthers <sam.struthers2000@gmail.com>
2025-07-29 15:24:36 -0600

    script for functions to run/deploy app

--- dashboard/testing_run.R
+++
@@ -1,8 +0,0 @@
-
-library(here)
-#get current WD and add dashboard to it
-path = paste0(here(), "/dashboard")
-#Set wd to dashboard to test as though in web deployment
-setwd(path)
-#run the app
-shiny::runApp(appDir = path )
