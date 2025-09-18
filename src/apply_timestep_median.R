8c6ce4dc0bc10f1a21dcf41062bf3bb9a3844839
SamStruthers <sam.struthers2000@gmail.com>
2025-09-18 13:48:13 -0600

    removing data_format, changing summarize -> mutate to preserve full df, changing dt_col

--- src/apply_timestep_median.R
+++ src/apply_timestep_median.R
@@ -7,22 +7,17 @@
 #' reduces the influence of short-term variability and outliers, while retaining the
 #' overall temporal patterns in the data.
 #' Users can submit either a single long-format dataframe containing multiple sites and parameters,
-#' or a list of dataframes where each dataframe contains data for a single site-parameter combination.
+#' or a map over a list of dataframes where each dataframe contains data for a single site-parameter combination.
 #'
 #' @param df Input data containing water quality measurements. Must include columns for
 #' site, datetime, parameter, and value.
-#' @param data_format The format of the input data. Options are either `"dataframe"` or `"list"`.
-#' If `"dataframe"` is selected, all data (multiple site-parameter combinations) are stored in
-#' a single long dataframe. If `"list"` is selected, data are stored in a list of dataframes,
-#' with each dataframe containing data for a single site-parameter combination.
-#' Default is `"dataframe"`.
 #' @param site_col The name of the column containing site identifiers. Default is `"site"`.
 #' @param parameter_col The name of the column containing parameter identifiers. Default is `"parameter"`.
-#' @param time_col The name of the column containing datetime information. Must be a `POSIXct`
-#' or otherwise compatible with `lubridate::round_date()`. Default is `"DT_round"`.
+#' @param dt_col The name of the column containing datetime information. Must be a `POSIXct`
+#' or otherwise compatible with `lubridate::floor_date()`. Default is `"DT_round"`.
 #' @param value_col The name of the column containing the values to be aggregated. Default is `"mean"`.
 #' @param new_value_col The name of the new column to store median-aggregated values.
-#' Default is `"smoothed_mean"`.
+#' Default is `"hourly_median"`.
 #' @param timestep The time resolution for aggregation, passed to `lubridate::floor_date()`.
 #' Common values include `"1 hour"`, `"1 day"`, `"15 minutes"`, etc. Default is `"1 hour"`.
 #'
@@ -37,12 +32,11 @@
 #' # Aggregate to hourly median
 #' result <- apply_timestep_median(
 #'   df = your_sensor_data,
-#'   data_format = "dataframe",
 #'   site_col = "site",
 #'   parameter_col = "parameter",
-#'   time_col = "DT_round",
+#'   dt_col = "DT_round",
 #'   value_col = "mean",
-#'   new_value_col = "smoothed_mean",
+#'   new_value_col = "hourly_median",
 #'   timestep = "1 hour"
 #' )
 #'
@@ -56,49 +50,25 @@
 
 
 apply_timestep_median <- function(df,
-                                  data_format = "dataframe",
                                   site_col = "site",
                                   parameter_col = "parameter",
-                                  time_col = "DT_round",
+                                  dt_col = "DT_round",
                                   value_col = "mean",
-                                  new_value_col = "smoothed_mean",
+                                  new_value_col = "hourly_median",
                                   timestep = "1 hour") {
-
-  # Function to add column if it doesn't exist
-  add_column_if_not_exists <- function(df, column_name, default_value = NA) {
-    if (!column_name %in% colnames(df)) {
-      df <- df %>% dplyr::mutate(!!sym(column_name) := default_value)
-    }
-    return(df)
+  # Check that dt_col is POSIXct
+  time_vals <- df[[dt_col]]
+  if (!lubridate::is.POSIXt(time_vals)) {
+    stop("Error: The datetime column must be POSIXct/POSIXt.")
   }
+  # Aggregate to specified timestep using median
+  df %>%
+    mutate(
+      DT_group = lubridate::floor_date(!!sym(dt_col), unit = timestep)
+    ) %>%
+    group_by(!!sym(site_col), !!sym(parameter_col), DT_group) %>%
+    mutate(!!sym(new_value_col) := median(!!sym(value_col), na.rm = TRUE)) %>%
+    ungroup()
 
-  if (data_format == "dataframe") {
-    data_smoothed <- df %>%
-      mutate(
-        .time_group = lubridate::round_date(!!sym(time_col), unit = timestep)
-      ) %>%
-      group_by(!!sym(site_col), !!sym(parameter_col), .time_group) %>%
-      summarise(!!sym(new_value_col) := median(!!sym(value_col), na.rm = TRUE),
-                .groups = "drop") %>%
-      rename(!!sym(time_col) := .time_group)
 
-    return(data_smoothed)
-
-  } else if (data_format == "list") {
-    data_smoothed <- purrr::map(df, ~ {
-      .x <- add_column_if_not_exists(.x, new_value_col)
-
-      .x %>%
-        mutate(.time_group = lubridate::floor_date(!!sym(time_col), unit = timestep)) %>%
-        group_by(!!sym(site_col), !!sym(parameter_col), .time_group) %>%
-        summarise(!!sym(new_value_col) := median(!!sym(value_col), na.rm = TRUE),
-                  .groups = "drop") %>%
-        rename(!!sym(time_col) := .time_group)
-    })
-
-    return(data_smoothed)
-
-  } else {
-    stop("Invalid data_format. Choose either 'dataframe' or 'list'.")
-  }
 }
