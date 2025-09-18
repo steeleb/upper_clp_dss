6aca49d0cbee95535c5a3e5b0edc74054211c7c7
SamStruthers <sam.struthers2000@gmail.com>
2025-09-18 13:48:43 -0600

    overflagging at low temps and sfm at mid range chla

--- src/apply_cleaning_filters.R
+++ src/apply_cleaning_filters.R
@@ -30,8 +30,10 @@ apply_cleaning_filters <- function(df, value_col = "mean", new_value_col = "mean
         !is.na(auto_flag) & parameter == "FDOM Fluorescence" & auto_flag == "outside of seasonal range" ~ !!sym(value_col), # FDOM sensor over flagged for seasonal range due to lack of data
         !is.na(auto_flag) & parameter == "Turbidity" & auto_flag == "outside of seasonal range" & !!sym(value_col) <= 10 & !!sym(value_col) != 0 ~ !!sym(value_col), # Turbidity sensor over flagged for seasonal range due to lack of data at lower values
         !is.na(auto_flag) & parameter == "Chl-a Fluorescence" & str_detect(auto_flag, regex("outside of seasonal range|slope violation", ignore_case = TRUE)) & !!sym(value_col) <= 1 ~ !!sym(value_col), # Chl-a sensor over flagged at lower ranged for seasonal thresholds or slope violations
+        !is.na(auto_flag) & parameter == "Chl-a Fluorescence" & auto_flag == "outside of seasonal range" & site == "sfm" & !!sym(value_col) <= 2 ~ !!sym(value_col), # Chl-a sensor over flagged at lower ranged for seasonal thresholds or slope violations
         !is.na(auto_flag) & auto_flag == "outside of seasonal range" & site == "chd" & parameter != "pH" ~ !!sym(value_col), #chambers is being overflagged across all parameters except pH
         !is.na(auto_flag) & auto_flag == "drift"  & parameter == "Turbidity" & !!sym(value_col) <= 15 ~ !!sym(value_col), # Drift below 15 NTU can be neglected (not cause for alarms)
+        !is.na(auto_flag) & auto_flag == "outside of seasonal range"  & parameter == "Temperature" & !!sym(value_col) <= 15 ~ !!sym(value_col), # overflagged temp at lower range
         is.na(auto_flag) & is.na(mal_flag) ~ !!sym(value_col),  # Otherwise keep original value
         TRUE ~ NA_real_ # if it is not one of these cases, set to NA
       ),
