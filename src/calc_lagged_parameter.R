270da4d6ee7f45cc6e1ae961f5a7ca0068de09f1
SamStruthers <sam.struthers2000@gmail.com>
2025-09-22 08:57:40 -0600

    correcting documentation and changing time period parsing to use str_split_i

--- src/calc_lagged_parameter.R
+++ src/calc_lagged_parameter.R
@@ -5,52 +5,47 @@
 #' columns, and creates a new column with the lagged values.
 #'
 #'
-#' @param df A data frame containing time series data with missing values
-#'   represented as `NA`.
+#' @param wide_df A dataframe in wide format with a column for each parameter,
+#'  a column for site identifiers, and a column for datetime information.
+#'  The datetime column must be of type `POSIXct` or numeric, or otherwise
 #' @param site_col A string giving the name of the column containing site
 #'   identifiers.
-#' @param parameter_col A string giving the name of the column containing
-#'   parameter identifiers.
+#' @param parameter_col A string specifying the parameter to apply lagging to. Default
+#'  is "Temperature".
 #' @param dt_col A string giving the name of the column containing datetime
 #'   information. Must be a `POSIXct` or numeric type, or otherwise convertible
 #'   to numeric.
-#' @param value_col A string giving the name of the column containing the values
-#'   to be gap filled.
-#' @param parameter A string specifying the parameter to apply lagging to. Default
-#'  is "Temperature".
-#'
 #' @param lag_fun A string specifying the lagging function to use. Should be one of
 #'  "mean", "median", "min", "max", "sd", or "var". Default is "median".
-#'
 #' @param time_period A string specifying the time period for lagging. Should be one of
-#'  "min", "hour", "day" & "week" with a number specifying the number that unit. Default is "4 hour".
+#'  "min", "hour", "day" & "week" with a number specifying the number that unit. Default is "12 hour".
 #'
 #' @param new_value_col A string giving the name of the new column to store the
-#'   gap-filled values. Default is `"lagged_mean"`.
+#'   gap-filled values. Default is `"lagged_median"`.
 #'
 #' @return A data frame with the original columns plus an additional column
-#'   containing the linearly interpolated values (or with the original column
-#'   replaced if `new_value_col` = `value_col`).
-#'
+#'   containing the lagged parameter values based on the `lag_fun`, `time_period` and `parameter_col`
+#'   specified. The new column is named according to the `new_value_col` parameter.
 #' @details
-#' This function uses grouped operations (`dplyr::group_by()`) by `site_col` and
-#' `parameter_col`. Within each group, linear interpolation is applied to the
-#' `value_col` based on the numeric representation of `dt_col` for the number
-#' of time intervals specified by `max_gap`
 #'
-#' @importFrom zoo na.approx
+#' This function uses the `slide_period_dbl` function from the `slider` package to compute lagged values.
+#' It groups the data by the specified site column and applies the lagging function over the defined time period (`time_period`).
+#' The function supports various lagging functions including mean, median, min, max, standard deviation, and variance.
+#' The function is designed to handle missing values appropriately based on the chosen lagging function but in all cases,
+#' does not require a complete window to perform calculations
 #'
 #'
 #'
 
 calc_lagged_parameter <- function(wide_df, site_col = "site", parameter_col = "Temperature",
                                   dt_col = "DT_round", lag_fun = "median",
-                                  time_period = "12 hour", new_value_col = "lagged_mean") {
+                                  time_period = "12 hour", new_value_col = "lagged_median") {
 
   #seperate time period by space
-  time_period_split <- strsplit(time_period, " ")[[1]]
-  time_period_num <- as.numeric(time_period_split[1])
-  # #parsing function
+  time_period_num <- as.numeric(str_split_i(time_period, " ", 1))
+  time_period_str <- str_split_i(time_period, " ", 2)
+
+    # #parsing function
   fn <- switch(lag_fun,
                "var" = stats::var,
                "mean" = mean,
@@ -76,7 +71,7 @@ calc_lagged_parameter <- function(wide_df, site_col = "site", parameter_col = "T
       !!sym(new_value_col) := slide_period_dbl(
         .x = !!sym(parameter_col), # use the specified parameter column
         .i = !!sym(dt_col), # use the time column for indexing
-        .period = time_period_split[2], # use the specified time unit
+        .period = time_period_str, # use the specified time unit
         .f = fn_used, # use match.fun to convert string to function
         .before = time_period_num, # look back the specified time period
         .complete = FALSE # do not require complete windows
